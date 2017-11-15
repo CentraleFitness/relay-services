ef5cdb85bb8015548a634e5124ee4da25bac4197
Léo POULIN <leo@goodbarber.net>
2017-11-14 23:03:55 -0800

    working on classes

--- display/gcontainer.py
+++ display/gcontainer.py
@@ -1,6 +1,10 @@
-from display.container import Container
-
 from PIL import Image
+
+from display.container import Container
+from display.objects.button import Button
+from display.objects.label import Label
+from display.objects.scrollbar import ScrollBar
+from display.objects.textbox import TextBox
 
 class GContainer(Container):
     """Short for 'Graphic Container'"""
@@ -9,4 +13,14 @@ class GContainer(Container):
         super().__init__()
 
     def translate(self) -> Image:
-        return Image.new('1', (128, 64), color=0)
+        render = Image.new('1', (128, 64), color=0)
+        for obj in self.objects:
+            if isinstance(obj, Label):
+                pass
+            elif isinstance(obj, TextBox):
+                pass
+            elif isinstance(obj, Button):
+                pass
+            elif isinstance(obj, ScrollBar):
+                pass
+        return render
---
+++ display/objects/scrollbar.py
@@ -0,0 +1,6 @@
+class ScrollBar(object):
+    """ScrollBar"""
+    def __init__(self, nb_pages: int, current: int = 1):
+        self.nb_pages = nb_pages
+        self.current = current
+        assert nb_pages > 0 and current > 0 and current <= nb_pages
