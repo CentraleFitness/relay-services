b5d0bbddfdd02a6fe3b3c20f21f50b661dc2e542
Léo POULIN <leoraphael.poulin@epitech.eu>
2017-11-20 10:58:49 -0800

    implementing the display

--- hardware/oled_128_64.py
+++ hardware/oled_128_64.py
@@ -1,6 +1,10 @@
+from PIL import Image, ImageDraw
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_GPIO.I2C as I2C
+import Adafruit_SSD1306
+
 from objects.graphics.gcontainer import GContainer
 
-from PIL import Image, ImageDraw
 
 SCREEN_SIZE = (128, 64)
 
@@ -8,13 +12,26 @@ class Oled_128_64(object):
     """description of class"""
 
     def __init__(self, i2c_bus=1, i2c_address=0x3c, *args, **kwargs):
-        self.display = None
+        self.display = Adafruit_SSD1306.SSD1306_128_64(
+            rst=1,
+            i2c_bus=i2c_bus,
+            i2c_address=i2c_address,
+            i2c=I2C)
+        self.display.begin()
+        self.display.clear()
         self.image = None
         self.size = SCREEN_SIZE
         self.content = kwargs.get('content', None)
 
     def display_content(self) -> None:
-        self.image.show()
+        ### Windows
+        # self.image.show()
+        ### Raspbian
+        if self.image:
+            self.display.image(self.image)
+            self.display.display()
+        else:
+            print("Nothing to display")
 
     def update_content(self) -> None:
         if self.content:
--- test.py
+++ test.py
@@ -6,37 +6,7 @@ from objects.graphics.gtextbox import *
 from objects.graphics.gcontainer import GContainer
 
 
-#L_pin = 27
-#R_pin = 23
-#C_pin = 4
-#U_pin = 17
-#D_pin = 22
-
-#A_pin = 5
-#B_pin = 6
-
-#INPUTS = {
-#    L_pin: False,
-#    R_pin: False,
-#    C_pin: False,
-#    U_pin: False,
-#    D_pin: False,
-#    A_pin: False,
-#    B_pin: False
-#}
-
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(A_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(B_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(L_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(R_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(U_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(D_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(C_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
 if __name__ == "__main__":
-
     joy1 = Joystick()
     a_button = Pushbutton('A', 5)
     b_button = Pushbutton('B', 6)
@@ -45,18 +15,7 @@ if __name__ == "__main__":
         print(joy1.get_inputs())
         print(a_button.get_status_update())
         print(b_button.get_status_update())
-        time.sleep(.1)
-
-    #while True:
-    #    for _input in INPUTS:
-    #        if not GPIO.input(_input) and not INPUTS[_input]:
-    #            INPUTS[_input] = not INPUTS[_input]
-    #            print("{} pressed".format(_input))
-    #        elif GPIO.input(_input) and INPUTS[_input]:
-    #            INPUTS[_input] = not INPUTS[_input]
-    #            print("{} released".format(_input))
-                
-    
+        time.sleep(.1)                    
     display = Oled_128_64(
         content=GContainer(
             SCREEN_SIZE,
