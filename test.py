73a2e0d9f39ca579083dd2487d2ee6094e3bbc5f
cf-hotspot-001 <root@cf-hotspot-001.local>
2017-11-20 19:24:43 +0000

    base menu ok

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import time
 
 from hardware.oled_128_64 import *
-from hardware.joystick import Joystick, Pushbutton
+from hardware.joystick import Joystick
+from hardware.pushbutton import Pushbutton, PBStatus
 from objects.graphics.gtextbox import *
 from objects.graphics.gcontainer import GContainer
 
@@ -11,19 +12,33 @@ if __name__ == "__main__":
     a_button = Pushbutton('A', 5)
     b_button = Pushbutton('B', 6)
 
-    while True:
-        print(joy1.get_inputs())
-        print(a_button.get_status_update())
-        print(b_button.get_status_update())
-        time.sleep(.1)                    
-    display = Oled_128_64(
-        content=GContainer(
-            SCREEN_SIZE,
-            (0, 0),
-            objects=[
-                GTextBox((100, 9), (0, 0), "Toto", x=1, y=-1),
-                GTextBox((100, 9), (0, 9), "Tata", x=1, y=-1),
-                GTextBox((100, 9), (0, 18), "Titi", x=1, y=-1)
-                ]))
+    menu1 = GContainer(
+        SCREEN_SIZE,
+        (0, 0),
+        objects=[
+            GTextBox((100, 9), (0, 0), "Toto", x=1, y=-1),
+            GTextBox((100, 9), (0, 9), "Tata", x=1, y=-1),
+            GTextBox((100, 9), (0, 18), "Titi", x=1, y=-1)
+        ])
+    display = Oled_128_64(content=menu1)
+    cursor = 0
+    menu1.objects[cursor % len(menu1.objects)].selected = True
     display.update_content()
     display.display_content()
+    try:
+        while True:
+            tmp = joy1.get_inputs()
+            if ('U_pin', PBStatus.RELEASED) in tmp:
+                menu1.objects[cursor % len(menu1.objects)].selected = False
+                cursor -= 1
+                menu1.objects[cursor % len(menu1.objects)].selected = True
+                display.update_content()
+                display.display_content()
+            elif ('D_pin', PBStatus.RELEASED) in tmp:
+                menu1.objects[cursor % len(menu1.objects)].selected = False
+                cursor += 1
+                menu1.objects[cursor % len(menu1.objects)].selected = True
+                display.update_content()
+                display.display_content()
+    except KeyboardInterrupt:
+        print("Ctrl-C")
