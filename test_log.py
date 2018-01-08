bc257c4974c4658378f55842c3243cf8da8d23e5
Léo POULIN <leoraphael.poulin@epitech.eu>
2018-01-07 16:15:07 -0800

    handler factory

--- test_log.py
+++ test_log.py
@@ -1,19 +1,5 @@
-#import sys
-#import logging
-import time
-
-#if __name__ == "__main__":
-#    sh = logging.StreamHandler(sys.stdout)
-#    formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s')
-#    formatter.default_time_format = '%H:%M:%S'
-#    formatter.default_msec_format = '%s.%03d'
-#    sh.setFormatter(formatter)
-#    logging.basicConfig(
-#        handlers=(sh, ),
-#        level=logging.DEBUG)
-#    logging.debug('test')
-
 import sys
+import time
 import utils.logger as logger
 
 if __name__ == "__main__":
--- utils/logger.py
+++ utils/logger.py
@@ -33,16 +33,10 @@ class Logger(object, metaclass=Singleton):
             '[%(asctime)s][%(levelname)s] %(message)s')
         self.format.default_time_format = '%H:%M:%S'
         self.format.default_msec_format = '%s.%03d'
+        self.__set = False
         return super().__init__(**kwargs)
 
-    def set_config(self):
-        """
-        Run the basic config command
-        """
-        assert self.level in LEVEL_DICT.keys()
-        logging.basicConfig(handlers=self.handlers, level=LEVEL_DICT[self.level])
-
-    def __add_handler_to_basicconfig(self, handler, new_level):
+    def __add_handler_to_basicconfig(self, handler, new_level) -> None:
         """
         Add the handler to the list of active ones and call basicConfig()
         Assertion is made when the handler is already in the list or
@@ -52,58 +46,65 @@ class Logger(object, metaclass=Singleton):
         self.handlers.append(handler)
         logging.basicConfig(handlers=self.handlers, level=LEVEL_DICT[new_level])
 
+    def _handler_factory(self, handlertype, *args, **kwargs) -> logging.Handler:
+        handler = handlertype(*args)
+        handler.setLevel(LEVEL_DICT[kwargs.get('level', 'notset')])
+        handler.setFormatter(self.format)
+        return handler
+
+    def set_config(self) -> None:
+        """
+        Run the basic config command using the handlers previously defined
+        This command should be used only once
+        """
+        assert self.level in LEVEL_DICT.keys() and not self.__set
+        logging.basicConfig(handlers=self.handlers, level=LEVEL_DICT[self.level])
+        self.__set = True
+
     def add_file_handler(self,  destfolder: str = "./logs/",
-                         nameformat: str = "%y%m%d_%H%M%S", **kwargs):
+                         nameformat: str = "%y%m%d_%H%M%S", **kwargs) -> None:
         """
         Give a file nameformat and a folder to write logs in a file
         The nameformat goes through time.strftime()
         kwargs:
-            level: update the logging level of the program
+            level: set the logging level of this specific handler
         """
-        handler = logging.FileHandler(
-            "{}/{}.log".format(destfolder, time.strftime(nameformat)))
-        handler.setLevel(logging.DEBUG)
-        handler.setFormatter(self.format)
-        #self.__add_handler_to_basicconfig(
-        #    handler, kwargs.get('level', self.level))
+        self.handlers.append(self._handler_factory(
+            logging.FileHandler,
+            "{}/{}.log".format(destfolder, time.strftime(nameformat)),
+            **kwargs))
 
-    def add_stream_handler(self, stream, **kwargs):
+    def add_stream_handler(self, stream, **kwargs) -> None:
         """
         Add a stream to output the logs
         kwargs:
-            level: update the logging level of the program
+            level: set the logging level of this specific handler
         """
-        handler = logging.StreamHandler(stream)
-        handler.setLevel(logging.DEBUG)
-        handler.setFormatter(self.format)
-        self.handlers.append(handler)
-        #self.__add_handler_to_basicconfig(
-        #    handler, kwargs.get('level', self.level))
+        self.handlers.append(self._handler_factory(
+            logging.StreamHandler,
+            stream,
+            **kwargs))
 
-    def add_udp_handler(self, host, port, **kwargs):
+    def add_udp_handler(self, host, port, **kwargs) -> None:
         """
-        Deprecated
+        Add a UDP handler (not really usefull)
+        kwargs:
+            level: set the logging level of this specific handler
         """
-        handler = logging.handlers.DatagramHandler(host, port)
-        handler.setLevel(logging.DEBUG)
-        handler.setFormatter(self.format)
-        #handler.send("Hi".encode())
-        #logging.getLogger().addHandler(handler)
-        #self.__add_handler_to_basicconfig(
-        #    handler, kwargs.get('level', self.curr))
+        self.handlers.append(self._handler_factory(
+            logging.handlers.DatagramHandler,
+            host, port,
+            **kwargs))
 
-    def add_syslog_handler(self, address=SYSLOG_ADDR, **kwargs):
+    def add_syslog_handler(self, address=SYSLOG_ADDR, **kwargs) -> None:
         """
         Add a SysLogHandler to the log output
         The basic UDP socket is used for the communication
         args:
             address: a tuple representing (host, port) or a string '/dev/log'
         kwargs:
-            level: update the logging level of the program
+            level: set the logging level of this specific handler
         """
-        handler = logging.handlers.SysLogHandler(address=address)
-        handler.setLevel(logging.DEBUG)
-        handler.setFormatter(self.format)
-        self.handlers.append(handler)
-        #self.__add_handler_to_basicconfig(
-        #    handler, kwargs.get('level', self.level))
+        self.handlers.append(self._handler_factory(
+            logging.handlers.SysLogHandler,
+            address=address, **kwargs))
