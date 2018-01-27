ae86fc8047d59f3eb31e125b23350944721ad1bd
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-01-26 22:27:11 -0800

    getmoduleid implemented

--- api_main.py
+++ api_main.py
@@ -1,6 +1,30 @@
 import requests
 from config.master import *
 
+class Dynamo:
+    def __init__(self, address, uuid, **kwargs):
+        self.address = address
+        self.uuid = uuid
+        self.session_id = None
+        self.t_prod = list()
+        return super().__init__(**kwargs)
+
+    def prod_sum(self) -> float:
+        mem =  sum(self.t_prod)
+        self.t_prod.clear()
+        return mem
+
+
+class BluetoothHandler:
+    def __init__(self, **kwargs):
+        self.address = 0x01
+        self.name = "BT Server"
+        self.mode = "Central"
+        self.type = "Server"
+        self.clients = {}
+        return super().__init__(**kwargs)
+
+
 class ClientHandler:
     def __init__(self):
         self.url = SERVER_URL
@@ -31,9 +55,20 @@ class ClientHandler:
         
 if __name__ == "__main__":
     client = ClientHandler()
+    modules = [
+        Dynamo(address, uuid) for address, uuid in
+        ((0x2, "001:001:001"), (0x3, "001:001:002"))
+        ]
 
     api_key = "0001-2083-a4b2-c00f"
-    modules = ['001:001:001', '001:001:002', '001:001:002']
 
     print("POST /getModuleId")
-    print("replied: {}", client.get_module_id(api_key, modules))
+    id_dict = client.get_module_id(api_key, (dynamo.uuid for dynamo in modules))
+    for dynamo in modules:
+        if dynamo.uuid in id_dict:
+            dynamo.session_id = id_dict[dynamo.uuid]
+            print("uuid: {}, session_id {}".format(dynamo.uuid,
+                                                   dynamo.session_id))
+        else:
+            print("missing session_id for module {}".format(dynamo.uuid))
+
