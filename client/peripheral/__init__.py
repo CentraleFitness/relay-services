312f150230a05624efd9635cb461bc7aeec7a35d
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-08-12 19:41:34 +0200

    deleted old import

--- client/peripheral/__init__.py
+++ client/peripheral/__init__.py
@@ -1,8 +1,3 @@
 import platform
 
 from .dynamo import Dynamo
-
-if platform.system() != 'Windows':
-    from .joystick import Joystick
-    from .oled_128_64 import Oled_128_64
-    from .pushbutton import Pushbutton
